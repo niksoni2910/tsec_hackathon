8829a978a4542e346a548d0b208ebfd40c8967ac
niksoni2910 <nikhilsoni2910@gmail.com>
2024-09-30 18:12:22 +0530

    conflicts resolved

--- tsec_hack/lib/main.dart
+++ tsec_hack/lib/main.dart
@@ -1,12 +1,5 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:tsec_hack/screens/bnav.dart';
-import 'package:tsec_hack/screens/recent_alert_list.dart';
-import 'package:tsec_hack/screens/transaction_page.dart';
-=======
-import 'package:tsec_hack/profile_page.dart';
-import 'package:tsec_hack/screens/bnav.dart';
->>>>>>> 32bba3edb2cb5b64683b2a641c2337758c2d70c8
 
 void main() {
   runApp(const MyApp());
